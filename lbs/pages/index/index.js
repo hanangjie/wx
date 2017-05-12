d3e250e3726144e6fd69a3c60b26aafe8704b4d8
hanangjie <hanangjie@gmail.com>
2017-05-12 15:34:40 +0800

    fice

--- lbs/pages/index/index.js
+++ lbs/pages/index/index.js
@@ -1,26 +1,28 @@
 //index.js
 //获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    markers: [{
+      iconPath: "local.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 30,
+      height: 30
+    }],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  onLoad:function(){
+    var _this=this;
+    wx.getLocation({
+        type: 'wgs84',
+        success: function (res) {
+            var speed = res.speed
+            var accuracy = res.accuracy
+            _this.setData({
+              latitude: res.latitude,
+              longitude: res.longitude
+            })
+        }
     })
   }
 })
